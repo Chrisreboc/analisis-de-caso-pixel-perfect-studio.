461d6be2709757021c6603404bd37a89a0dcff4d
Cris <cr27.rebolledo@gmail.com>
2026-01-16 22:59:54 -0300

    Se importan algunos estilos a styles.scss, se agrega un index.js y 3 imágenes para el futuro slideshow.

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+// MODULO 1 TABHEADER
+
+// MODULO 2 SLIDESHOW
+
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      let slides = document.getElementsByClassName("slideshow__slide");
+      let dots = document.getElementsByClassName("slideshow__dot");
+
+      if (n > slides.length) { slideIndex = 1; }
+      if (n < 1) { slideIndex = slides.length; }
+
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("slideshow__dot--active");
+      }
+
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].classList.add("slideshow__dot--active");
+    }
