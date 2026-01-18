5202ac4db084e04a37d1a3d1a6c68c331f612603
Cris <cr27.rebolledo@gmail.com>
2026-01-17 23:27:54 -0300

    se añade el módulo 1, slideshow de imágenes, con funcionalidad de su index y estilos correspondientes.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,30 +3,30 @@
 // MODULO 2 SLIDESHOW
 
 let slideIndex = 1;
-    showSlides(slideIndex);
+showSlides(slideIndex);
 
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    function showSlides(n) {
-      let slides = document.getElementsByClassName("slideshow__slide");
-      let dots = document.getElementsByClassName("slideshow__dot");
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slideshow__slide");
+  let dots = document.getElementsByClassName("slideshow__dot");
 
-      if (n > slides.length) { slideIndex = 1; }
-      if (n < 1) { slideIndex = slides.length; }
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
 
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (let i = 0; i < dots.length; i++) {
-        dots[i].classList.remove("slideshow__dot--active");
-      }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("slideshow__dot--active");
+  }
 
-      slides[slideIndex - 1].style.display = "block";
-      dots[slideIndex - 1].classList.add("slideshow__dot--active");
-    }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("slideshow__dot--active");
+}
